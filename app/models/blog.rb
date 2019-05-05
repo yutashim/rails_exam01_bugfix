95f8e9c63f74e12168e323eaf490c42663f1f8df
yuji91 <yuji91.1227@gmail.com>
2019-05-05 21:25:14 +0900

    Fix add dependent: :destroy to association

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,7 +1,9 @@
 class Blog < ApplicationRecord
   # ErrorCause: undefined method `comments' for Blog, association typo
   # FixReason: fix typo, has_many :comment to :comments
-  has_many :comments
+  # ErrorCause: FOREIGN KEY constraint failed, when destroy blog with comments
+  # FixReason: add dependent: :destroy to association
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true
   validates :content, presence: true
