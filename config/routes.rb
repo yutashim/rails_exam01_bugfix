16f1e868af5b682c96a1ab1a1d1dceefd112ad27
yuji91 <yuji91.1227@gmail.com>
2019-05-05 20:47:16 +0900

    Fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :blog do
+  # ErrorCause: No route matches [GET] "/blogs"
+  # FixReason: Change URI Pattern blog to blogs
+  resources :blogs do
     resources :comments, only: [:create, :destroy]
   end
 end
