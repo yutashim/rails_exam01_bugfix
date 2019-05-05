4beca7f6cdfff89619778f32ccdcbcaacd9bfba2
yuji91 <yuji91.1227@gmail.com>
2019-05-05 20:50:54 +0900

    Fix association typo

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,7 @@
 class Blog < ApplicationRecord
-  has_many :comment
+  # ErrorCause: undefined method `comments' for Blog, association typo
+  # FixReason: fix typo, has_many :comment to :comments
+  has_many :comments
 
   validates :title, presence: true
   validates :content, presence: true
