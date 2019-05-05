7e3d3f7854ff21a53248442c61f3107fbe737006
yuji91 <yuji91.1227@gmail.com>
2019-05-05 21:14:59 +0900

    Fix set parameter to redirect_to argument

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,7 +9,9 @@ class CommentsController < ApplicationController
   def destroy
     @comment = @blog.comments.find(params[:id])
     @comment.destroy
-    redirect_to
+    # ErrorCause: no parameter set to redirect_to argument
+    # FixReason: set parameter to redirect_to argument
+    redirect_to blog_path(@blog)
   end
 
   private
