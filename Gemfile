16cf1b649448706e472833559b0bc09ba6bd72c1
yuta <az7xj5xn@yahoo.co.jp>
2021-02-10 15:31:48 +0900

    Fix: ルーティングの修正

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.3'
 
-gem 'rails', '~> 5.2.3'
+gem 'rails', '~> 5.2.4'
 gem 'sqlite3'
 gem 'puma', '~> 3.11'
 gem 'sass-rails', '~> 5.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :blog do
+  resources :blogs do
+    #エラーの原因: :blogを基にルーティングを生成していたため、urlがblogsコントローラを参照しなし。
+    #修正の意図: :blogsに変更して、blogsコントローラに対応
     resources :comments, only: [:create, :destroy]
   end
 end
